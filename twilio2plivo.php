602be3b9d27705035e7d31403565300f5cb6572e
Clint Berry <clint+git@system-11.com>
2012-07-19 22:10:16 -0600

    Added base classes to helper

---
+++ twilio2plivo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Twilio2Plivo Helper file
+ * Version 0.1.0
+ * Author: Clint Berry (http://clintberry.com)
+ */
+
+
+class Services_Twilio() {
+    
+}
+
+class Services_Twilio_Twiml() {
+    
+}
